7cb099c400f74128b25609446b05bcd7747c44e6
bema-fabiyi <ayomiposifab13@gmail.com>
2025-01-29 16:50:06 +0100

    added link

---
+++ bema_hng/android/app/src/main/kotlin/com/example/bema_hng/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bema_hng
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ bema_hng/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'BEMA HNG'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final Uri url = Uri.parse('https://hng.tech/hire/mobile-ui-ux-developers');
+
+  Future<void> openUrl() async {
+    try {
+      await launchUrl(url);
+    } catch (e) {}
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            openUrl();
+          },
+          child: Text('Open HNG link'),
+        ),
+      ),
+    );
+  }
+}
